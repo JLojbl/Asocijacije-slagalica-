03b1fc2a27ac9a9814d54ddcee83eee0fce72a67
minaandrejevic2007-ai <minaandrejevic2007@gmail.com>
2026-05-29 09:30:59 +0200

    osnovni izgled

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+               InitializeComponent();
+    
+    this.Size = new Size(1620, 920);
+    // ILI odmah fullscreen:
+    this.WindowState = FormWindowState.Maximized;
+    this.FormBorderStyle = FormBorderStyle.None;
+    this.Bounds = Screen.PrimaryScreen.Bounds;
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close(); // ili vrati normalan prikaz
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
